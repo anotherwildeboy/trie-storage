e09f80a6ad55f593063b1e77b0c56385e2cdf283
James <38592822+james-1032@users.noreply.github.com>
2019-05-18 15:35:24 +0100

    Update trie-storage.js

--- trie-storage.js
+++ trie-storage.js
@@ -1,74 +1,105 @@
-var treenode = function(a) {
-  this.key = a;
-  this.pointer = 0;
-  this.data;
-  this.branches = [];
-  this.alive = !0;
-};
-module.exports.root = function() {
-  this.length = 0;
-  this.rootnode = new treenode("root");
-  this.store = storeData;
-  this.get = retrieveData;
-  this.remove = removeData;
-};
-function checkDone(a, b) {
-  return b.length - 1 <= a ? !0 : !1;
+let treenode = class {
+	constructor(key) {
+		this.key = key;
+		this.pointer = 0;
+		this.data;
+		this.branches = [];
+		this.alive = true;
+	}
 }
-function storeData(a, b) {
-  this.length++;
-  recursiveInsert(this.rootnode, 0, b, a);
+module.exports.root = class {
+	constructor() {
+		this.length = 0;
+		this.rootnode = new treenode("root");
+		this.store = storeData;
+		this.get = retrieveData;
+		this.remove = removeData;
+	}
 }
-function retrieveData(a) {
-  return recursiveRetrieve(this.rootnode, 0, a);
+
+function checkDone(count, datakey) {
+	return (datakey.length - 1 <= count ? true : false);
 }
-function removeData(a) {
-  (a = recursiveRemove(this.rootnode, 0, a)) && this.length--;
-  return a;
+
+function storeData(data, datakey) {
+	this.length++;
+	recursiveInsert(this.rootnode, 0, datakey, data);
 }
-function recursiveInsert(a, b, d, e) {
-  for (var c = d.substring(b, b + 1), f = 0; f < a.pointer; f++) {
-    if (a.branches[f].key == c) {
-      if (checkDone(b, d)) {
-        return a.branches[f].data = e, a.branches[f].alive = !0, a.branches;
-      }
-      a = a.branches[f];
-      b++;
-      return recursiveInsert(a, b, d, e);
-    }
-  }
-  c = new treenode(d.substring(b, b + 1));
-  a.branches.push(c);
-  a.pointer++;
-  a = c;
-  if (checkDone(b, d)) {
-    return a.data = e, a.branches;
-  }
-  b++;
-  return recursiveInsert(a, b, d, e);
+
+function retrieveData(datakey) {
+	return recursiveRetrieve(this.rootnode, 0, datakey);
 }
-function recursiveRetrieve(a, b, d) {
-  for (var e = d.substring(b, b + 1), c = 0; c < a.pointer; c++) {
-    if (a.branches[c].key == e) {
-      if (checkDone(b, d) && a.branches[c].alive) {
-        return a.branches[c].data;
-      }
-      a = a.branches[c];
-      b++;
-      return recursiveRetrieve(a, b, d);
-    }
-  }
+
+function removeData(datakey) {
+	let result = recursiveRemove(this.rootnode, 0, datakey);
+	if (result) {
+		this.length--;
+	}
+	return result;
 }
-function recursiveRemove(a, b, d) {
-  for (var e = d.substring(b, b + 1), c = 0; c < a.pointer; c++) {
-    if (a.branches[c].key == e) {
-      if (checkDone(b, d)) {
-        return b = a.branches[c].data, a.branches[c].alive = !1, b;
-      }
-      a = a.branches[c];
-      b++;
-      return recursiveRetrieve(a, b, d);
-    }
-  }
+
+function recursiveInsert(currentNode, count, datakey, data) {
+	let subkey = datakey.substring(count, count + 1);
+	for (let i = 0; i < currentNode.pointer; i++) {
+		if (currentNode.branches[i].key == subkey) {
+			if (checkDone(count, datakey)) {
+				currentNode.branches[i].data = data;
+				currentNode.branches[i].alive = true;
+				return currentNode.branches;
+			}
+			else {
+				currentNode = currentNode.branches[i];
+				count++;
+				return recursiveInsert(currentNode, count, datakey, data);
+			}
+		}
+	}
+	let newEmptyNode = new treenode(datakey.substring(count, count + 1));
+	currentNode.branches.push(newEmptyNode);
+	currentNode.pointer++;
+	currentNode = newEmptyNode;
+	if (checkDone(count, datakey)) {
+		currentNode.data = data;
+		return currentNode.branches;
+	}
+	else {
+		count++;
+		return recursiveInsert(currentNode, count, datakey, data);
+	}
 }
-;
+
+function recursiveRetrieve(currentNode, count, datakey) {
+	let subkey = datakey.substring(count, count + 1);
+	for (let i = 0; i < currentNode.pointer; i++) {
+		if (currentNode.branches[i].key == subkey) {
+			if (checkDone(count, datakey) && currentNode.branches[i].alive) {
+				return currentNode.branches[i].data;
+			}
+			else {
+				currentNode = currentNode.branches[i];
+				count++;
+				return recursiveRetrieve(currentNode, count, datakey);
+			}
+		}
+	}
+	return undefined;
+}
+
+function recursiveRemove(currentNode, count, datakey) {
+	let subkey = datakey.substring(count, count + 1);
+	for (let i = 0; i < currentNode.pointer; i++) {
+		if (currentNode.branches[i].key == subkey) {
+			if (checkDone(count, datakey)) {
+				let data = currentNode.branches[i].data;
+				currentNode.branches[i].alive = false;
+				return data;
+			}
+			else {
+				currentNode = currentNode.branches[i];
+				count++;
+				return recursiveRetrieve(currentNode, count, datakey);
+			}
+		}
+	}
+	return undefined;
+}
